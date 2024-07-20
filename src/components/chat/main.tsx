8672b15ce4e1bef845e53d60f5e92dda3d8c6a67
Abbdurahman <dreydare16@gmail.com>
2024-07-20 16:14:48 +0100

    adjusted full screen view on mobile

--- src/components/chat/main.tsx
+++ src/components/chat/main.tsx
@@ -4,7 +4,7 @@ import { Menu } from "../../assets";
 const Main: React.FC = function () {
   return (
     <section className="bg-white h-screen w-full overflow-hidden pt-14 pb-24 relative md:pt-3">
-      <div className="w-full p-4 fixed top-0 left-0 shadow-lg cursor-pointer md:hidden">
+      <div className="w-full p-4 fixed top-0 left-0 shadow-lg cursor-pointer md:hidden bg-white">
         <Menu />
       </div>
 
